a6df4debfc775ee3a9e5b8c6e1e5c0f6e6bf46ce
sciolkowski <szymon.ciolkowski@eengine.pl>
2017-09-14 10:12:59 +0200

    Move setting the proper collection name to ORMongo

--- api/web/models/product.ex
+++ api/web/models/product.ex
@@ -4,40 +4,35 @@ defmodule Perseids.Product do
   @collection_name "products"
   @filterable_params ["category_ids", "color","size"]
 
-  # TODO przerobić wyciąganie filtrów na listing na wykorzystujące współbieżność
-  # zmienne "available_params" w funkcjach "find"
-
-  def find([{:lang, lang} | tail] = opts) do
-    find(lang, tail)
-  end
-
-  def find(lang, [{:keywords, keywords}, _] = opts) do
-    #  available_params = extract_filters(@filterable_params, %{"keywords" => [keywords]}, %{}, lang)
+  # TODO filters need hard optimization, so they're disabled for now
+  
+  def find([{:keywords, keywords} | _] = opts) do
+    #  available_params = extract_filters(@filterable_params, %{"keywords" => [keywords]}, %{}, opts[:lang])
     available_params = [] #### Temporarly disable filters
-    lang |> response(opts, available_params, opts)
+    response(opts, available_params, opts)
   end
 
-  def find(lang, [{:filter, filters}, _] = opts) do
-    #  available_params = extract_filters(@filterable_params, filters, %{}, lang)
+  def find([{:filter, filters} | _] = opts) do
+    #  available_params = extract_filters(@filterable_params, filters, %{}, opts[:lang])
     available_params = [] #### Temporarly disable filters
-    lang |> response(opts, available_params, filters)
+    response(opts, available_params, filters)
   end
 
-  def find(lang, [_] = opts) do
-    #  available_params = extract_filters(@filterable_params, %{}, %{}, lang)
+  def find([_] = opts) do
+    #  available_params = extract_filters(@filterable_params, %{}, %{}, opts[:lang])
     available_params = [] #### Temporarly disable filters
-    lang |> response(opts |> Keyword.put_new(:filter, %{}), available_params, %{})
+    response(opts |> Keyword.put_new(:filter, %{}), available_params, %{})
   end
 
-  def response(lang, opts, available_params, count_opts) do
-    ORMongo.collection_name(lang, @collection_name)
-    |> ORMongo.find(opts)
+  def response(opts, available_params, count_opts) do
+    @collection_name
+    |> ORMongo.find_with_lang(opts)
     |> list_response(available_params, count_opts)
   end
 
-  def find_one(lang, source_id) do
-    ORMongo.collection_name(lang, @collection_name)
-    |> ORMongo.find([source_id: source_id])
+  def find_one(source_id) do
+    @collection_name
+    |> ORMongo.find_with_lang([source_id: source_id])
     |> item_response
   end
 
@@ -62,15 +57,15 @@ defmodule Perseids.Product do
     values = case current_param do
      "category_ids" ->
        where = Map.drop(conditions, [current_param])
-       ORMongo.collection_name(lang, @collection_name)
-       |> ORMongo.find(filter: where)
+       @collection_name
+       |> ORMongo.find_with_lang(filter: where, lang: lang)
        |> Enum.map(fn(e) -> e["categories"] end)
        |> List.flatten
        |> Enum.map(fn(e) -> e["id"] end)
      _ ->
        where = Map.drop(conditions, ["params." <> current_param])
-       ORMongo.collection_name(lang, @collection_name)
-       |> ORMongo.find(filter: where)
+       @collection_name
+       |> ORMongo.find_with_lang(filter: where, lang: lang)
        |> Enum.map(fn(e) -> e["params"][current_param] end)
        |> List.flatten
     end
