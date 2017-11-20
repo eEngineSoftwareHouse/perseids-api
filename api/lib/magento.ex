fc35935256a3c90f9e2b4266ca7f5067d961d5a5
Szymon Ciołkowski <szymon.ciolkowski@gmail.com>
2017-11-20 15:05:43 +0100

    Make Magento requests with specific store_view in URL (#82)

--- api/lib/magento.ex
+++ api/lib/magento.ex
@@ -6,17 +6,17 @@ defmodule Magento do
     password: Application.get_env(:perseids, :magento)[:admin_password]
   }
 
-  def online? do
-    case post("integration/customer/token") do
+  def online?(store_view) do
+    case store_view |> post("integration/customer/token") do
       { :ok, _response } -> true
       { :error, _reason } -> false
     end
   end
 
-  def store_configs do
-    case admin_token() do
+  def store_configs(store_view) do
+    case admin_token(store_view) do
       { :ok, token } ->
-        case get("store/storeConfigs", [{"Authorization", "Bearer #{token}"}]) do
+        case store_view |> get("store/storeConfigs", [{"Authorization", "Bearer #{token}"}]) do
           { :ok, response } -> response.body
           { :error, reason } -> reason
         end
@@ -26,77 +26,77 @@ defmodule Magento do
 
   end
 
-  def admin_token do
-    case post("integration/admin/token", Poison.encode!(@magento_admin_credentials)) do
+  def admin_token(store_view) do
+    case store_view |> post("integration/admin/token", Poison.encode!(@magento_admin_credentials)) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
-  def customer_token(credentials) do
-    case post("integration/customer/token", Poison.encode!(credentials)) do
+  def customer_token(store_view, credentials) do
+    case store_view |> post("integration/customer/token", Poison.encode!(credentials)) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
 
-  def customer_info(token) do
-    case get("customers/me", [{"Authorization", "Bearer #{token}"}]) do
+  def customer_info(store_view, token) do
+    case store_view |> get("customers/me", [{"Authorization", "Bearer #{token}"}]) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
-  def address_info(token, address_type) do
-    case get("customers/me/#{address_type}Address", [{"Authorization", "Bearer #{token}"}]) do
+  def address_info(store_view, token, address_type) do
+    case store_view |> get("customers/me/#{address_type}Address", [{"Authorization", "Bearer #{token}"}]) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
-  def create_account(params) do
-    {:ok, token} = admin_token()
-    case post("customers", Poison.encode!(params), [{"Authorization", "Bearer #{token}"}]) do
+  def create_account(store_view, params) do
+    {:ok, token} = admin_token(store_view)
+    case store_view |> post("customers", Poison.encode!(params), [{"Authorization", "Bearer #{token}"}]) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
-  def update_account(params, customer_id: customer_id, customer_token: customer_token) do
-    {:ok, token} = admin_token()
-    params = %{ "customer" => set_customer_email(customer_token, params) }
-    case put("customers/#{customer_id}", Poison.encode!(params), [{"Authorization", "Bearer #{token}"}]) do
+  def update_account(store_view, params, customer_id: customer_id, customer_token: customer_token) do
+    {:ok, token} = admin_token(store_view)
+    params = %{ "customer" => set_customer_email(store_view, customer_token, params) }
+    case store_view |> put("customers/#{customer_id}", Poison.encode!(params), [{"Authorization", "Bearer #{token}"}]) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
-  defp set_customer_email(token, params) do
-    case customer_info(token) do
+  defp set_customer_email(store_view, token, params) do
+    case customer_info(store_view, token) do
       {:ok, customer} -> params["customer"] |> Map.put("email", customer["email"])
       {:error, reason} -> params
     end
   end
 
-  def reset_password(%{"email" => _email, "website_id" => _website_id} = params) do
-    {:ok, token} = admin_token()
-    case put("customers/password", Poison.encode!(params |> Map.put_new("template", "email_reset")), [{"Authorization", "Bearer #{token}"}]) do
+  def reset_password(store_view, %{"email" => _email, "website_id" => _website_id} = params) do
+    {:ok, token} = admin_token(store_view)
+    case store_view |> put("customers/password", Poison.encode!(params |> Map.put_new("template", "email_reset")), [{"Authorization", "Bearer #{token}"}]) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
-  def reset_password(%{"password" => password, "password_confirmation" => _password_confirmation, "token" => token, "email" => email}) do
-    case put("base/password", Poison.encode!(%{"newPassword" => password, "resetToken" => token, "email" => email})) do
+  def reset_password(store_view, %{"password" => password, "password_confirmation" => _password_confirmation, "token" => token, "email" => email}) do
+    case store_view |> put("base/password", Poison.encode!(%{"newPassword" => password, "resetToken" => token, "email" => email})) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
-  def product_stock(sku) do
-    {:ok, token} = admin_token()
-    case get("stockItems/#{sku}", [{"Authorization", "Bearer #{token}"}]) do
+  def product_stock(store_view, sku) do
+    {:ok, token} = admin_token(store_view)
+    case store_view |> get("stockItems/#{sku}", [{"Authorization", "Bearer #{token}"}]) do
       { :ok, response } -> magento_response(response)
       { :error, reason } -> %{errors: [reason]}
     end
@@ -104,13 +104,13 @@ defmodule Magento do
 
   # magento nie pozwala zapytać o stock wielu produktów jednocześnie, więc dla każdego idzie request,
   # do zmiany po rozbudowie API magento naszą wtyczką
-  def stock_items(sku_list) do
-    {:ok, token} = admin_token()
-    products_qty(sku_list, %{}, token)
+  def stock_items(store_view, sku_list) do
+    {:ok, token} = admin_token(store_view)
+    products_qty(store_view, sku_list, %{}, token)
   end
 
-  defp products_qty([head | tail], accumulator, token) do
-    accumulator = case get("stockItems/#{head}", [{"Authorization", "Bearer #{token}"}]) do
+  defp products_qty(store_view, [head | tail], accumulator, token) do
+    accumulator = case store_view |> get("stockItems/#{head}", [{"Authorization", "Bearer #{token}"}]) do
       { :ok, response } ->
         case magento_response(response) do
           { :ok, stock } -> Map.put_new(accumulator, "#{head}", stock["qty"])
@@ -118,10 +118,10 @@ defmodule Magento do
         end
       _ -> accumulator
     end
-    products_qty(tail, accumulator, token)
+    products_qty(store_view, tail, accumulator, token)
   end
 
-  defp products_qty([], accumulator, _token) do
+  defp products_qty(_store_view, [], accumulator, _token) do
     accumulator
   end
 
@@ -151,16 +151,17 @@ defmodule Magento do
     String.replace(str, "%#{index}", "#{word}")
   end
 
-  defp get(url, headers) do
-    HTTPoison.get(@magento_host <> url, [{"Content-Type", "application/json"} | headers], @magento_timeout)
+  defp get(store_view, url, headers) do
+    HTTPoison.get(magento_api_url(url, store_view), [{"Content-Type", "application/json"} | headers], @magento_timeout)
   end
 
-  defp post(url, params \\ Poison.encode!(%{}), headers \\ []) do
-    HTTPoison.post(@magento_host <> url, params, [{"Content-Type", "application/json"} | headers], @magento_timeout)
+  defp post(store_view, url, params \\ Poison.encode!(%{}), headers \\ []) do
+    HTTPoison.post(magento_api_url(url, store_view), params, [{"Content-Type", "application/json"} | headers], @magento_timeout)
   end
 
-  defp put(url, params, headers \\ []) do
-    HTTPoison.put(@magento_host <> url, params, [{"Content-Type", "application/json"} | headers], @magento_timeout)
+  defp put(store_view, url, params, headers \\ []) do
+    HTTPoison.put(magento_api_url(url, store_view), params, [{"Content-Type", "application/json"} | headers], @magento_timeout)
   end
-
+  
+  defp magento_api_url(url, store_view \\ "plpl"), do: @magento_host <> "rest/" <> store_view <> "/V1/" <> url
 end
