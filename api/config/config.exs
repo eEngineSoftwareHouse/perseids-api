ebeba5099a5f9ca0e6a618ff0bee0cdc9887c421
sciolkowski <szymon.ciolkowski@eengine.pl>
2018-02-05 16:39:21 +0100

    Multi-currency PayU based on request language header

--- api/config/config.exs
+++ api/config/config.exs
@@ -31,8 +31,8 @@ config :phoenix, :generators,
 # Configure Magento Access
 config :perseids, :magento,
   magento_api_endpoint: System.get_env("MAGENTO_API_ENDPOINT"),
-  admin_username: System.get_env("MAGENTO_ADMIN_USERNAME"),
-  admin_password: System.get_env("MAGENTO_ADMIN_PASSWORD")
+  admin_username:       System.get_env("MAGENTO_ADMIN_USERNAME"),
+  admin_password:       System.get_env("MAGENTO_ADMIN_PASSWORD")
 
 # Configure GetResponse
 config :perseids, :get_response,
@@ -42,22 +42,50 @@ config :perseids, :get_response,
   api_campaign_token_en: System.get_env("GETRESPONSE_API_CAMPAIGN_TOKEN_EN")
 
 # Configure PayU
-config :perseids, :payu,
-  api_url: System.get_env("PAYU_API_URL"),
-  pos_id: System.get_env("PAYU_POS_ID"),
-  client_id: System.get_env("PAYU_CLIENT_ID"),
-  client_secret: System.get_env("PAYU_CLIENT_SECRET"),
-  second_key: System.get_env("PAYU_SECOND_KEY"),
-  notify_url: System.get_env("PAYU_NOTIFY_URL"),
-  continue_url: System.get_env("PAYU_CONTINUE_URL")
+config :perseids, :payu_pln,
+  api_url:        System.get_env("PAYU_API_URL"),
+  notify_url:     System.get_env("PAYU_NOTIFY_URL"),
+  continue_url:   System.get_env("PAYU_CONTINUE_URL"),
+  pos_id:         System.get_env("PLN_PAYU_POS_ID"),
+  client_id:      System.get_env("PLN_PAYU_CLIENT_ID"),
+  client_secret:  System.get_env("PLN_PAYU_CLIENT_SECRET"),
+  second_key:     System.get_env("PLN_PAYU_SECOND_KEY")
+
+config :perseids, :payu_eur,
+  api_url:        System.get_env("PAYU_API_URL"),
+  notify_url:     System.get_env("PAYU_NOTIFY_URL"),
+  continue_url:   System.get_env("PAYU_CONTINUE_URL"),
+  pos_id:         System.get_env("EUR_PAYU_POS_ID"),
+  client_id:      System.get_env("EUR_PAYU_CLIENT_ID"),
+  client_secret:  System.get_env("EUR_PAYU_CLIENT_SECRET"),
+  second_key:     System.get_env("EUR_PAYU_SECOND_KEY")  
+
+config :perseids, :payu_gbp,
+  api_url:        System.get_env("PAYU_API_URL"),
+  notify_url:     System.get_env("PAYU_NOTIFY_URL"),
+  continue_url:   System.get_env("PAYU_CONTINUE_URL"),
+  pos_id:         System.get_env("GBP_PAYU_POS_ID"),
+  client_id:      System.get_env("GBP_PAYU_CLIENT_ID"),
+  client_secret:  System.get_env("GBP_PAYU_CLIENT_SECRET"),
+  second_key:     System.get_env("GBP_PAYU_SECOND_KEY")
+
+config :perseids, :payu_usd,
+  api_url:        System.get_env("PAYU_API_URL"),
+  notify_url:     System.get_env("PAYU_NOTIFY_URL"),
+  continue_url:   System.get_env("PAYU_CONTINUE_URL"),
+  pos_id:         System.get_env("USD_PAYU_POS_ID"),
+  client_id:      System.get_env("USD_PAYU_CLIENT_ID"),
+  client_secret:  System.get_env("USD_PAYU_CLIENT_SECRET"),
+  second_key:     System.get_env("USD_PAYU_SECOND_KEY")
+
 
 # Configure PayPal
 config :perseids, :paypal,
-  api_url: System.get_env("PAYPAL_API_URL"),
-  client_id: System.get_env("PAYPAL_CLIENT_ID"),
+  api_url:       System.get_env("PAYPAL_API_URL"),
+  client_id:     System.get_env("PAYPAL_CLIENT_ID"),
   client_secret: System.get_env("PAYPAL_CLIENT_SECRET"),
-  return_url: System.get_env("PAYPAL_RETURN_URL"),
-  cancel_url: System.get_env("PAYPAL_CANCEL_URL")
+  return_url:    System.get_env("PAYPAL_RETURN_URL"),
+  cancel_url:    System.get_env("PAYPAL_CANCEL_URL")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- api/lib/payu.ex
+++ api/lib/payu.ex
@@ -1,27 +1,17 @@
 defmodule PayU do
-  @payu_api_url Application.get_env(:perseids, :payu)[:api_url]
   @payu_api_version_endpoint "api/v2_1/"
-  @payu_pos_id Application.get_env(:perseids, :payu)[:pos_id]
-  @payu_notify_url Application.get_env(:perseids, :payu)[:notify_url]
-  @payu_continue_url Application.get_env(:perseids, :payu)[:continue_url]
-  @payu_second_key Application.get_env(:perseids, :payu)[:second_key]
   @payu_timeout [connect_timeout: 30000, recv_timeout: 30000, timeout: 30000]
-  @payu_credentials %{
-    grant_type: "client_credentials",
-    client_id: Application.get_env(:perseids, :payu)[:client_id],
-    client_secret: Application.get_env(:perseids, :payu)[:client_secret]
-  }
 
-  def oauth_token do
-    body = "grant_type=#{@payu_credentials.grant_type}&client_id=#{@payu_credentials.client_id}&client_secret=#{@payu_credentials.client_secret}"
-    case HTTPoison.post(@payu_api_url <> "pl/standard/user/oauth/authorize", body, [{"Content-Type", "application/x-www-form-urlencoded"}], @payu_timeout) do
+  def oauth_token(currency) do
+    body = "grant_type=client_credentials&client_id=#{payu_config(currency, :client_id)}&client_secret=#{payu_config(currency, :client_secret)}"
+    case HTTPoison.post(payu_config(currency, :api_url) <> "pl/standard/user/oauth/authorize", body, [{"Content-Type", "application/x-www-form-urlencoded"}], @payu_timeout) do
       { :ok, response } -> payu_response(response)
       { :error, reason } -> {:error, reason}
     end
   end
 
-  def check_sig(json_body, signature) do
-    :crypto.hash(:md5, json_body <> @payu_second_key)
+  def check_sig(currency, json_body, signature) do
+    :crypto.hash(:md5, json_body <> payu_config(currency, :second_key))
     |> Base.encode16(case: :lower)
     |> Kernel.==(signature)
   end
@@ -29,10 +19,10 @@ defmodule PayU do
   def place_order(%{"email" => email, "products" => products, "shipping" => shipping, "lang" => lang, "currency" => currency, "shipping_price" => shipping_price, "order_total_price" => order_total_price} = order) do
     shipping = Perseids.Shipping.find_one(source_id: shipping, lang: lang)
     payu_order = %{
-      notifyUrl: @payu_notify_url,
-      continueUrl: @payu_continue_url,
+      notifyUrl: payu_config(currency, :notify_url),
+      continueUrl: payu_config(currency, :continue_url),
       customerIp: "127.0.0.1", # Needed by PayU, don't know why
-      merchantPosId: @payu_pos_id,
+      merchantPosId: payu_config(currency, :pos_id),
       description: email <> "-" <> BSON.ObjectId.encode!(order["_id"]),
       currencyCode: currency,
       totalAmount: payu_format_price(order_total_price + shipping_price),
@@ -42,7 +32,7 @@ defmodule PayU do
       |> Kernel.++([shipping |> payu_prepare_shipping(shipping_price)])
     }
 
-    case post("orders", Poison.encode!(payu_order), [], follow_redirect: true) do
+    case post(currency, "orders", Poison.encode!(payu_order), [], follow_redirect: true) do
       {:ok, response} -> manage_response(response, order)
       {:error, message} -> IO.inspect message; raise "PayU order request error"
     end
@@ -95,11 +85,11 @@ defmodule PayU do
     end
   end
 
-  defp post(url, params, headers, options \\ []) do
+  defp post(currency, url, params, headers, options \\ []) do
     HTTPoison.post(
-      @payu_api_url <> @payu_api_version_endpoint <> url,
+      payu_config(currency, :api_url) <> @payu_api_version_endpoint <> url,
       params,
-      [{"Content-Type", "application/json"}, {"Authorization", "Bearer #{token()}"} | headers],
+      [{"Content-Type", "application/json"}, {"Authorization", "Bearer #{token(currency)}"} | headers],
       @payu_timeout ++ options
     )
   end
@@ -112,9 +102,14 @@ defmodule PayU do
     ""
   end
 
-  defp token() do
-    oauth_token()
+  defp token(currency) do
+    oauth_token(currency)
     |> token()
   end
+
+  defp payu_config("PLN", key), do: Application.get_env(:perseids, :payu_pln)[key]
+  defp payu_config("USD", key), do: Application.get_env(:perseids, :payu_usd)[key]
+  defp payu_config("EUR", key), do: Application.get_env(:perseids, :payu_eur)[key]
+  defp payu_config("GBP", key), do: Application.get_env(:perseids, :payu_gbp)[key]
 
 end
