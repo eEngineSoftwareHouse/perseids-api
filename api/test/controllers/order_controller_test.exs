a9340c8bae011d735b031e8b187929e6c64f12d9
sciolkowski <szymon.ciolkowski@eengine.pl>
2018-03-02 15:05:34 +0100

    Order listing display tests for logged in / logged out users

--- api/test/controllers/order_controller_test.exs
+++ api/test/controllers/order_controller_test.exs
@@ -1,21 +1,34 @@
 defmodule Perseids.OrderControllerTest do
-  use Perseids.ConnCase
+  use Perseids.ConnCase, async: true
+
+  import Perseids.Gettext
 
   alias Perseids
   alias Perseids.Order
 
-  # @create_attrs %{}
-  # @update_attrs %{}
-  # @invalid_attrs %{}
+  @valid_credentials %{"email" => "szymon.ciolkowski@eengine.pl", "password" => "Tajnafraza12"}
+  
+  defp logged_in(conn), do: conn |> Perseids.ConnCase.login(@valid_credentials, "pl_pln")
+  defp guest(conn), do: conn |> Perseids.ConnCase.guest("pl_pln")
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    {:ok, conn: conn}
   end
+  
+  describe "Orders listing" do
+    test "guest user cannot display orders list", %{conn: conn} do
+      conn = conn 
+      |> guest
+      |> get(order_path(conn, :index))
+      assert json_response(conn, 401) == %{"errors" => [gettext "You do not have sufficient permissions."]}
+    end
 
-  describe "index" do
-    test "lists all index", %{conn: conn} do
-      conn = get conn, order_path(conn, :index)
-      assert json_response(conn, 200) == []
+    test "logged in user can display his orders", %{conn: conn} do
+      conn = conn 
+      |> logged_in
+      |> get(order_path(conn, :index))
+      assert json_response(conn, 200) == %{"count" => 0, "orders" => []}
     end
   end
+  
 end
