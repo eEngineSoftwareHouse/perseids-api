6a3773cb4be0593214813c8a20567d239fff6061
gorzki <rafal.rozniakowski@gmail.com>
2018-03-05 15:48:46 +0100

    created simple user login/register/update and password reset tests

---
+++ api/test/controllers/session_controller_test.exs
@@ -0,0 +1,34 @@
+defmodule Perseids.SessionControllerTest do
+  use Perseids.ConnCase, async: true
+
+  @valid_credentials %{"email" => "szymon.ciolkowski@eengine.pl", "password" => "Tajnafraza12"}
+  
+  defp logged_in(conn), do: conn |> Perseids.ConnCase.login(@valid_credentials, "pl_pln")
+  # defp logout(conn), do: conn |> Perseids.ConnCase.logout(%{})
+
+  setup %{conn: conn} do
+    conn = put_req_header(conn, "content-type", "application/json")
+    {:ok, conn: conn}
+  end
+  describe "Session -" do
+    test "create/2", %{conn: conn} do
+      conn = conn
+      |> logged_in
+      assert get_req_header(conn, "authorization") != nil
+      assert conn.assigns.currency == "PLN"
+      assert conn.assigns.lang == "pl_pln"
+      assert conn.assigns.locale == "pl"
+      assert conn.assigns.store_view == "plpl"
+    end
+
+    # test "destroy", %{conn: conn} do
+    #   conn = conn
+    #   |> logged_in
+    #   IO.inspect conn
+    #   assert get_req_header(conn, "authorization") != nil
+    #   logout(conn)
+    #   IO.inspect conn
+    #   assert get_req_header(conn, "authorization") == [] 
+    # end
+  end
+end
