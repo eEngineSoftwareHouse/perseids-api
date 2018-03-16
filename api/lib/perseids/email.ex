020a428efc8cfca622ca41fea2d6ae8512bfa9c8
sciolkowski <szymon.ciolkowski@eengine.pl>
2018-03-16 16:57:52 +0100

    Put image into mail content

--- api/lib/perseids/email.ex
+++ api/lib/perseids/email.ex
@@ -9,15 +9,28 @@ defmodule Perseids.Email do
     |> text_body(content)
   end
 
-  def compliant_form(%{"email" => from, "order_id" => order_id, "image" => image, "_id" => complaint_id, "comment" => comment } = _params) do
+  def compliant_form(%{"email" => from, "order_id" => order_id, "_id" => complaint_id, "comment" => comment } = complaint) do
     new_email()
     |> to(Application.get_env(:perseids, :contact_form)[:email])
     |> from(from)
-    |> subject("Reklamacja - ManyMornings.com")
+    |> subject("Reklamacja (nr zam. #{order_id}) - ManyMornings.com")
+    |> maybe_image_in_html_body?(complaint["image"], order_id, comment)
+  end
+
+  defp maybe_image_in_html_body?(conn, nil, order_id, comment) do
+    conn
     |> html_body("
       <h1>Reklamacja do zamówienia nr #{order_id}</h1>
       <p>#{comment}</p>
-      <img src='#{image}' />
+    ")
+  end
+
+  defp maybe_image_in_html_body?(conn, image_url, order_id, comment) do
+    conn
+    |> html_body("
+      <h1>Reklamacja do zamówienia nr #{order_id}</h1>
+      <p>#{comment}</p>
+      <img src='#{image_url}' />
     ")
   end
 end
