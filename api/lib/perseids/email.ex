4ec7493bfc2684b66b663266efde1dc32a5957e0
sciolkowski <szymon.ciolkowski@eengine.pl>
2018-03-16 16:39:01 +0100

    Send email with complaint to shop admin

--- api/lib/perseids/email.ex
+++ api/lib/perseids/email.ex
@@ -8,4 +8,16 @@ defmodule Perseids.Email do
     |> subject("Wiadomość z formularza kontaktowego ManyMornings")
     |> text_body(content)
   end
+
+  def compliant_form(%{"email" => from, "order_id" => order_id, "image" => image, "_id" => complaint_id, "comment" => comment } = _params) do
+    new_email()
+    |> to(Application.get_env(:perseids, :contact_form)[:email])
+    |> from(from)
+    |> subject("Reklamacja - ManyMornings.com")
+    |> html_body("
+      <h1>Reklamacja do zamówienia nr #{order_id}</h1>
+      <p>#{comment}</p>
+      <img src='#{image}' />
+    ")
+  end
 end
