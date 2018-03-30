abd884d14318ba6aa3a250c925d208ff198343d3
gorzki <rafal.rozniakowski@gmail.com>
2018-03-30 12:06:32 +0200

    prepare tests for CircleCI

--- api/test/support/independent_database.ex
+++ api/test/support/independent_database.ex
@@ -17,12 +17,16 @@ defmodule Perseids.IndependentDatabase do
   ]
 
   @list_of_products [
-    "/product_1.json",
-    "/product_2.json",
-    "/product_3.json",
-    "/product_valid_order.json",
-    "/product_free_regular.json",
-    "/product_free_low.json"
+    "/product_1.json", #source_id : "434"
+    "/product_2.json", #source_id : "51"
+    "/product_3.json", #source_id : "111"
+    "/product_4.json", #source_id : "119"
+    "/product_5.json", #source_id : "143"
+    "/product_6.json", #source_id : "203"
+    "/product_7.json", #source_id : "107"
+    "/product_valid_order.json", #source_id : "455"
+    "/product_free_regular.json", #source_id : "458"
+    "/product_free_low.json" #source_id : "155"
   ]
 
   @list_of_tresholds [
@@ -39,6 +43,7 @@ defmodule Perseids.IndependentDatabase do
   def initialize(lang) do
     Mongo.delete_many(:mongo, "orders", %{})
     Mongo.delete_many(:mongo, "#{lang}_threshold", %{})
+    Mongo.delete_many(:mongo, "#{lang}_products", %{})
     Mongo.delete_many(:mongo, "#{lang}_discount", %{})
 
     create_discount(lang)
